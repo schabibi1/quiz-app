5a90388ed376b2279c37968fc2e478c6632601cc
Arisa Fukuzaki <arisagse@gmail.com>
2024-12-21 15:58:55 +0100

    add: fetch questions

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState } from "react";
+import { NhostProvider } from "@nhost/nextjs";
+import { nhost } from './lib/nhost'
 
-export default function Home() {
+const getQuestions = `
+  query {
+    questions {
+      question
+      answers {
+        answer
+      }
+    }
+  }
+`;
+
+function App() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <NhostProvider nhost={nhost}>
+      <Home />
+    </NhostProvider>
   );
 }
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      setLoading(true);
+      const { data, error } = await nhost.graphql.request(getQuestions);
+
+      setQuestions(data.questions);
+      setLoading(false);
+    }
+
+    fetchQuestions();
+  }, []);
+
+  return (
+    <main>
+      <article>
+        <section>
+          <h1>Quiz Game App</h1>
+          <h2>Rules</h2>
+          <p>Have fun to play with anyone! Choose correct answers to score high and brag about it!</p>
+        </section>
+          
+        <section>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {questions.map((singleQuestion, index) => (
+                <li key={index}>
+                  <p>Q: {singleQuestion.question}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+      </article>
+    </main>
+  );
+}
+
+export default App;
