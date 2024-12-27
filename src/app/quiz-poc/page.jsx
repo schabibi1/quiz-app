9d3f989e6a9e539f2f4dc8cdece407a3b7f99125
Arisa Fukuzaki <arisagse@gmail.com>
2024-12-27 21:10:20 +0100

    add: functioning POST req & res with scores from Lambda res

---
+++ src/app/quiz-poc/page.jsx
@@ -0,0 +1,194 @@
+'use client'
+import { useEffect, useState } from "react";
+import { NhostProvider } from "@nhost/nextjs";
+import { nhost } from '../lib/nhost'
+
+const getQuiz = `
+  query {
+	questions {
+	  question
+	  id
+	  answers {
+		answer
+		id
+	  }
+	}
+  }
+`;
+
+const initRequestState = [
+  {
+	question_id: "",
+	answer_id: "",
+  }
+]
+
+const initScoreState = {
+  score: 0,
+}
+
+function Quiz() {
+  return (
+	<NhostProvider nhost={nhost}>
+	  <QuizHome />
+	</NhostProvider>
+  );
+}
+
+function QuizHome() {
+  const [loading, setLoading] = useState(true);
+  const [answers, setAnswers] = useState([]);
+  // const [score, setScore] = useState(initScoreState);
+  const [score, setScore] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  const [players, setPlayers] = useState([
+	{ name: 'Player 1', score: 0 },
+	{ name: 'Player 2', score: 0 },
+  ]);
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+  const [requestData, setRequestData] = useState(initRequestState);
+  useEffect(() => {
+	async function fetchQuestions() {
+	  setLoading(true);
+
+	  const { data } = await nhost.graphql.request(getQuiz);
+	  setQuestions(data.questions);
+	  setAnswers(data.questions);
+	  setLoading(false);
+	};
+	fetchQuestions();
+  }, []);
+
+  const handleAnswerSelect = (answer) => {
+	setSelectedAnswer(answer);
+  };
+
+  const currentQuestion = questions[currentQuestionIndex];
+  const currentQuizInfo = answers[currentQuestionIndex];
+  // const correctAnswer = rightAnswers[currentQuestionIndex].correctAnswer;
+  const currentPlayer = players[currentPlayerIndex];
+
+  const handleSubmitAnswer = async () => {
+	console.log('selectedAnswer: ', selectedAnswer);
+	
+	if (selectedAnswer !== null) {// 🚨 Review this condition
+	  const updatedPlayers = [...players];
+	  // updatedPlayers[currentPlayerIndex].score += 1;
+	  updatedPlayers[currentPlayerIndex].score = score.score;
+	  setPlayers(updatedPlayers);
+	  setScore(score.score);// Not mutating score
+	  console.log('score: ', score);// TODO: Mutate score
+	}
+
+	// // Temporary commented out to narrow down the error spot for Array.splice
+	// if (currentQuestionIndex < questions.length - 1) {
+	//   setCurrentQuestionIndex(currentQuestionIndex + 1);
+	//   setSelectedAnswer(null);
+	// } else {
+	//   setQuizCompleted(true);
+	// }
+	// setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
+  };
+
+  // 🚨 POST req is not triggered -> Only triggered when testing in Postman
+  const handleSubmit = async (e) => {
+	e.preventDefault();
+	// setRequestData in handleChange below mutates the requestData state
+	console.log(requestData)
+
+	const res = await fetch('http://localhost:3000/api/quiz', {
+	  method: 'POST',
+	  headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify([requestData]),
+	});
+	// returns the NextResponse.json() reponse from the POST route
+	const result = await res.json();
+	console.log('result: ', result);
+  }
+
+  const handleChange = (e) => {
+	e.preventDefault();
+	setRequestData((prevData) => {
+	  // Replacing existing element to mutate the original answer_id
+	  const updatedPrevData = Object.assign(prevData, {
+		question_id: currentQuestion.id,
+		answer_id: e.target.value
+	  });
+	  console.log('updatedPrevData: ', updatedPrevData);
+	  return updatedPrevData;
+	});
+
+	// Forcing to go to the next Q to narrow down the error spot
+	// Copied from handleSubmitAnswer()
+	if (currentQuestionIndex < questions.length - 1) {
+	  setCurrentQuestionIndex(currentQuestionIndex + 1);
+	  setSelectedAnswer(null);
+	} else {
+	  setQuizCompleted(true);
+	}
+	setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
+  };
+
+  if (quizCompleted) {
+	return (
+	  <div>
+		<h2 className="text-2xl font-semibold text-white my-6">Quiz Completed!</h2>
+		{players.map((player, index) => (
+		  <p
+			key={index}
+			className="text-xl font-semibold text-white/60"
+		  >
+			{/* Update player.score (i.e. score.score) */}
+			{player.name}: {player.score} out of {questions.length}
+		  </p>
+		))}
+	  </div>
+	);
+  }
+
+  return (
+	<section>
+	  {loading ? (
+		<p className="text-xl font-semibold text-white/60">Loading...</p>
+	  ) : (
+	  <div className="flex flex-col space-y-4 w-full mb-8">
+		<p className="text-xl font-semibold text-white/60">Player: {currentPlayer.name}</p>
+		<h2 className="text-2xl font-semibold text-white my-6">{currentQuestion.question}</h2>
+		  <div className="flex flex-col space-y-4 w-full">
+			{currentQuizInfo.answers.map((a) => (
+			  <label className="flex items-center w-full py-4 pl-5 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5 hover:bg-white/15" key={a.id}>
+				<input
+				  type="radio"
+				  name="answer_id"
+				  value={a.id}
+				  checked={selectedAnswer === a.id}
+				  onChange={() => handleAnswerSelect(a.id)}
+				  onClick={handleChange}
+				  onSubmit={handleSubmit}
+				  className="w-6 h-6"
+				/>
+				{a.answer}
+			  </label>
+			))}
+		  </div>
+		<button
+		  // onSubmit={handleSubmit}
+		  onClick={handleSubmitAnswer}
+		  disabled={!selectedAnswer || loading}
+		  className={!selectedAnswer ? "w-full py-3 bg-indigo-500/75 rounded-lg font-semibold my-6" : "w-full py-3 bg-indigo-500/75 hover:bg-indigo-500 rounded-lg font-semibold my-6"}
+		>
+		  {loading ? 'Submitting...' : 'Next'}
+		</button>
+	  </div>
+	  )}
+	</section>
+  );
+}
+
+export default Quiz;
--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -1,197 +1,103 @@
-'use client'
-import { useEffect, useState } from "react";
-import { NhostProvider } from "@nhost/nextjs";
-import { nhost } from '../lib/nhost'
+import { useState } from 'react';
 
-const getQuiz = `
-  query {
-    questions {
-      question
-      id
-      answers {
-        answer
-        id
-      }
-    }
-  }
-`;
-
-const initRequestState = [
-  {
-    question_id: "",
-    answer_id: "",
-  }
-]
-
-const initScoreState = {
-  score: 0,
-}
-
-function Quiz() {
-  return (
-	<NhostProvider nhost={nhost}>
-	  <QuizHome />
-	</NhostProvider>
-  );
-}
-
-function QuizHome() {
-  const [loading, setLoading] = useState(true);
-  const [questions, setQuestions] = useState([]);
+const QuizForm = () => {
   const [answers, setAnswers] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [quizCompleted, setQuizCompleted] = useState(false);
-  const [players, setPlayers] = useState([
-    { name: 'Player 1', score: 0 },
-    { name: 'Player 2', score: 0 },
-  ]);
-  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
-  const [requestData, setRequestData] = useState(initRequestState);
-  const [scores, setScores] = useState(initScoreState);
+  const [score, setScore] = useState(null);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [wrongAnswers, setWrongAnswers] = useState([]);
 
-  useEffect(() => {
-    async function fetchQuestions() {
-      setLoading(true);
-
-      const { data } = await nhost.graphql.request(getQuiz);
-      setQuestions(data.questions);
-      setAnswers(data.questions);
-      setLoading(false);
-    };
-    fetchQuestions();
-  }, []);
-
-  const handleAnswerSelect = (answer) => {
-    setSelectedAnswer(answer);
-  };
-
-  const currentQuestion = questions[currentQuestionIndex];
-  const currentQuizInfo = answers[currentQuestionIndex];
-  // const correctAnswer = rightAnswers[currentQuestionIndex].correctAnswer;
-  const currentPlayer = players[currentPlayerIndex];
-
-  const handleSubmitAnswer = async () => {
-    console.log('selectedAnswer: ', selectedAnswer);
-    
-    if (selectedAnswer !== null) {// 🚨 Review this condition
-      const updatedPlayers = [...players];
-      // updatedPlayers[currentPlayerIndex].score += 1;
-      updatedPlayers[currentPlayerIndex].score = scores.score;
-      setPlayers(updatedPlayers);
-      setScores(scores.score);// Not mutating scores
-      console.log('scores: ', scores);// TODO: Mutate scores
-    }
-
-    // // Temporary commented out to narrow down the error spot for Array.splice
-    // if (currentQuestionIndex < questions.length - 1) {
-    //   setCurrentQuestionIndex(currentQuestionIndex + 1);
-    //   setSelectedAnswer(null);
-    // } else {
-    //   setQuizCompleted(true);
-    // }
-    // setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
+  const handleAnswerChange = (questionId, answerId) => {
+    setAnswers((prevAnswers) => {
+      const existingAnswer = prevAnswers.find((a) => a.question_id === questionId);
+      console.log('existingAnswer: ', existingAnswer);
+      
+      if (existingAnswer) {
+        existingAnswer.answer_id = answerId;
+      } else {
+        prevAnswers.push({ question_id: questionId, answer_id: answerId });
+      }
+      console.log('prevAnswers: ', prevAnswers);
+      return [...prevAnswers];
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // setRequestData in handleChange below mutates the requestData state
-    console.log(requestData)
 
-    const res = await fetch('http://localhost:3000/api/quiz', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify([requestData]),
-    });
-    // returns the NextResponse.json() reponse from the POST route
-    const result = await res.json();
-    console.log('result: ', result);
-  }
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setRequestData((prevData) => {
-      // // Replacing existing element to mutate the original answer_id
-      // prevData.splice(0, 1, {
-      //   question_id: currentQuestion.id,
-      //   answer_id: e.target.value
-      // });
-
-      const updatedPrevData = Object.assign(prevData, {
-        question_id: currentQuestion.id,
-        answer_id: e.target.value
+    try {
+      const response = await fetch('/api/quiz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ solutions: answers }),
       });
-      console.log('updatedPrevData: ', updatedPrevData);
-      return updatedPrevData;
-    });
+      console.log('response: ', response); 
+      console.log('answers: ', answers);
 
-    // Forcing to go to the next Q to narrow down the error spot
-    // Copied from handleSubmitAnswer()
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setSelectedAnswer(null);
-    } else {
-      setQuizCompleted(true);
+      if (!response.ok) {
+        throw new Error('Error submitting answers.');
+      }
+
+      const data = await response.json();
+      setScore(data.score);
+      setCorrectAnswers(data.correctAnswers);
+      setWrongAnswers(data.wrongAnswers);
+    } catch (error) {
+      console.error('Error:', error);
     }
-    setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
   };
 
-  if (quizCompleted) {
-    return (
-      <div>
-        <h2 className="text-2xl font-semibold text-white my-6">Quiz Completed!</h2>
-        {players.map((player, index) => (
-          <p
-            key={index}
-            className="text-xl font-semibold text-white/60"
-          >
-            {/* Update player.score (i.e. scores.score) */}
-            {player.name}: {player.score} out of {questions.length}
-          </p>
-        ))}
-      </div>
-    );
-  }
-
   return (
-    <section>
-      {loading ? (
-        <p className="text-xl font-semibold text-white/60">Loading...</p>
-      ) : (
-      <div className="flex flex-col space-y-4 w-full mb-8">
-        <p className="text-xl font-semibold text-white/60">Player: {currentPlayer.name}</p>
-        <h2 className="text-2xl font-semibold text-white my-6">{currentQuestion.question}</h2>
-          <div className="flex flex-col space-y-4 w-full">
-            {currentQuizInfo.answers.map((a) => (
-              <label className="flex items-center w-full py-4 pl-5 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5 hover:bg-white/15" key={a.id}>
-                <input
-                  type="radio"
-                  name="answer_id"
-                  value={a.id}
-                  checked={selectedAnswer === a.id}
-                  onChange={() => handleAnswerSelect(a.id)}
-                  onClick={handleChange}
-                  className="w-6 h-6"
-                />
-                {a.answer}
-              </label>
-            ))}
-          </div>
-        <button
-          onSubmit={handleSubmit}
-          // onClick={handleSubmitAnswer}
-          disabled={!selectedAnswer || loading}
-          className={!selectedAnswer ? "w-full py-3 bg-indigo-500/75 rounded-lg font-semibold my-6" : "w-full py-3 bg-indigo-500/75 hover:bg-indigo-500 rounded-lg font-semibold my-6"}
-        >
-          {loading ? 'Submitting...' : 'Next'}
-        </button>
-      </div>
-      )}
-    </section>
-  );
-}
+    <div>
+      <h2>Quiz</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Q1: How many planets are in our solar system?</h3>
+          <select
+            onChange={(e) =>
+              handleAnswerChange('a6d7a14c-58f0-4f33-9bf9-92580b6e1aa3', e.target.value)
+            }
+          >
+            <option value="4c19daac-3069-4f78-93c3-950d44ab2d74">6</option>
+            <option value="1939dd41-d73e-4bc8-bea3-c53d094eff66">8 Correct one</option>
+          </select>
+        </div>
 
-export default Quiz;
+        <div>
+          <h3>Q2: Which 'The Lord of the Rings' movie has the highest IMDb rating?</h3>
+          <select
+            onChange={(e) =>
+              handleAnswerChange('2dd95c9b-a7fe-47a8-8f78-a92caf3de955', e.target.value)
+            }
+          >
+            <option value="bcc2909e-3b24-4b25-b2da-311ad4102dcf">The Fellowship of the Ring</option>
+            <option value="7ad7c745-af78-4d02-b4fa-4e2d3608d176">The Return of the King, Correct one</option>
+          </select>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {score !== null && (
+        <div>
+          <h3>Your Score: {score}</h3>
+          <h4>Correct Answers:</h4>
+          <ul>
+            {correctAnswers.map((answer) => (
+              <li key={answer.question_id}>{answer.question_id}</li>
+            ))}
+          </ul>
+          <h4>Wrong Answers:</h4>
+          <ul>
+            {wrongAnswers.map((answer) => (
+              <li key={answer.question_id}>{answer.question_id}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QuizForm;
